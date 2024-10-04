0fbb416f1fafba8e167aa7a9cfc684f4d8d8d13b
Yoshida.Eiji <yoshida.eiji@trans-cosmos.co.jp>
2024-10-04 17:08:24 +0900

    33:46

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="h-1/2 md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between">
+      <Link href="/" className="font-bold text-xl">
+        MASSIMO
+      </Link>
+      <p>&copy; ALL RIGHTS RESERVED.</p>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
