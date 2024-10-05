c711c55141055afb87e5e9be0f6b192cca5bb282
eiji yoshida <eijiyoshida1912@gmail.com>
2024-10-05 18:46:58 +0900

    1:01:10

--- src/app/components/Offer.tsx
+++ src/app/components/Offer.tsx
@@ -1,7 +1,17 @@
+import Image from "next/image";
 import React from "react";
 
 const Offer = () => {
-  return <div>Offer</div>;
+  return (
+    <div className="bg-black h-screen flex flex-col md:flex-row">
+      {/* TEXT CONTAINER */}
+      <div className="flex-1"></div>
+      {/* IMAGE CONTAINER */}
+      <div className="flex-1 w-full relative">
+        <Image src="/offerProduct.png" alt="" fill className="object-contain" />
+      </div>
+    </div>
+  );
 };
 
 export default Offer;
